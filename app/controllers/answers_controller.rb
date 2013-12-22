585a633579efe4071a3b69a0f38ba71e2ef2cc1d
huangtw <huang0422@gmail.com>
2013-12-22 22:24:09 +0800

    Disable "close question when there are more than 5 answers"

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,19 +9,6 @@ class AnswersController < ApplicationController
 
     answer = @question.answers.create!(params[:answer])
 
-    @question.reload
-
-    if not(@question.answers.slice(0,5).include? answer)
-    	answer.destroy
-    	redirect_to @question, :notice => "Answer closed!" 
-    	return
-    end
-
-    if @question.answers.count >= 5
-    	@question.closed = true
-    	@question.save
-    end
-
     redirect_to @question, :notice => "Answer created!" 
   end
 end
